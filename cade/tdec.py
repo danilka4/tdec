9b79c6f026c56383123043e672cdc3db02ed423b
Daniel Palamarchuk <danielpalamarchuk@gmail.com>
2023-08-29 13:54:27 -0400

    Converting in process

--- cade/tdec.py
+++ cade/tdec.py
@@ -3,7 +3,9 @@
 """Main module."""
 
 import gensim
-from gensim.models.word2vec import Doc2Vec, Word2Vec
+from gensim.models.doc2vec import Doc2Vec
+from gensim.models.word2vec import Word2Vec
+from gensim.models import doc2vec
 
 import os
 import numpy as np
@@ -102,41 +104,45 @@ class TDEC:
             return gensim.utils.RULE_DISCARD
 
     def train_model(self, sentences):
-        model = Doc2Vec()
+        model = None
         if self.compass == None or self.init_mode != "copy":
             if self.mode == "dm":
                 model = Doc2Vec(size=self.size, alpha=self.static_alpha, iter=self.static_iter,
                              negative=self.negative,
                              window=self.window, min_count=self.min_count, workers=self.workers)
             elif self.mode == "dbow":
-                model = Word2Vec(sg=1, vector_size=self.size, alpha=self.static_alpha, epochs=self.static_iter,
+                model = Doc2Vec(dm=0, dbow_words=1, size=self.size, alpha=self.static_alpha, epochs=self.static_iter,
                                  negative=self.negative,
                                  window=self.window, min_count=self.min_count, workers=self.workers)
+            else:
+                return Exception('Set "mode" to be "dm" or "dbow"')
             model.build_vocab(sentences, trim_rule=self.internal_trimming_rule if self.compass != None else None)
         if self.compass != None:
             model = self.initialize_from_compass(model)
         model.train(sentences, total_words=sum([len(s) for s in sentences]), epochs=model.epochs, compute_loss=True)
         return model
 
-    def train_compass(self, compass_text, overwrite=False, save=False):
-        compass_exists = os.path.isfile(os.path.join(self.opath, "compass.model"))
+    def train_compass(self, compass_text, keep_dv = False, overwrite=False, save=False, compass_name="compass.model"):
+        compass_exists = os.path.isfile(os.path.join(self.opath, compass_name))
         if compass_exists and not overwrite:
             print("Compass is being loaded from file.")
-            self.compass = gensim.models.word2vec.Word2Vec.load(os.path.join(self.opath, "compass.model"))
+            self.compass = Doc2Vec.load(os.path.join(self.opath, compass_name))
             self.gvocab = self.compass.wv.vocab
             return
         sentences = gensim.models.word2vec.PathLineSentences(compass_text)
+        #[doc2vec.TaggedDocument(doc, [key]) for key, doc in texts.items()]
         sentences.input_files = [s for s in sentences.input_files if not os.path.basename(s).startswith('.')]
         print("Training the compass from scratch.")
         if compass_exists:
             print("Current saved compass will be overwritten after training")
         self.compass = self.train_model(sentences)
-
+        if not keep_dv:
+            self.compass.dv = gensim.models.KeyedVectors(vector_size=100)
         if save:
-            self.compass.save(os.path.join(self.opath, "compass.model"))
+            self.compass.save(os.path.join(self.opath, compass_name))
         self.gvocab = self.compass.wv.vocab
 
-    def train_slice(self, slice_text, save=False):
+    def train_slice(self, slice_text, slice_titles=None, save=False):
         """
         Training a slice of text
         :param slice_text:
@@ -147,7 +153,11 @@ class TDEC:
             return Exception("Missing Compass")
         print("Training embeddings: slice {}.".format(slice_text))
 
-        sentences = gensim.models.word2vec.LineSentence(slice_text)
+        sentences = None
+        if slice_titles:
+            sentences = [doc2vec.TaggedDocument(doc, [title]) for doc, title in zip(slice_text, slice_titles)]
+        else:
+            sentences = [doc2vec.TaggedDocument(doc, [i]) for i, doc in enumerate(slice_text)]
         model = self.train_model(sentences)
 
         model_name = os.path.splitext(os.path.basename(slice_text))[0]
